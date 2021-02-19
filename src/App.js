6d1f2092ba7a41cf8c574e0308f82245868230a1
John Bang <jeebeeng@gmail.com>
2021-02-19 01:07:05 -0500

    Add search bar

--- src/App.js
+++ src/App.js
@@ -1,38 +1,75 @@
-import React, { useState } from 'react';
-import { pokemon } from './data';
+import React, { useState, useEffect } from 'react';
+import { data } from './data';
+import './index.css';
 
 function App() {
-  const [data, setData] = useState(pokemon);
+  const [name, setName] = useState('');
+  const [list, setList] = useState(data);
+
+  useEffect(() => {
+    if (name === '') {
+      setList(data);
+    } else {
+    }
+  }, [name]);
+
   return (
     <div>
       <header>
         <h1>Pokemon Weakness Lookup</h1>
       </header>
-      <List data={data} />
+      <form className="form">
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+      </form>
+      <ul className="card-list">
+        {list.map((item) => {
+          return (
+            <li>
+              <Card data={item} />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
 
-const List = ({ data }) => {
+const Card = ({ data }) => {
+  const { name, id, weaknesses, resistances, immunities } = data;
   return (
-    <>
-      {data.map((item) => {
-        const { name, id, weaknesses } = item;
-        return (
-          <article key={id}>
-            <h2>{name}</h2>
-            <article>
-              <h3>Weaknesses</h3>
-              <ul>
-                {weaknesses.map((type, index) => {
-                  return <li key={index}>{type}</li>;
-                })}
-              </ul>
-            </article>
-          </article>
-        );
-      })}
-    </>
+    <article className="info-card" key={id}>
+      <h2>{name}</h2>
+      <article>
+        <h3>Weaknesses</h3>
+        <ul className="type-list">
+          {weaknesses.map((type, index) => {
+            return <li key={index}>{type}</li>;
+          })}
+        </ul>
+      </article>
+      <article>
+        <h3>Resistances</h3>
+        <ul className="type-list">
+          {resistances.map((type, index) => {
+            return <li key={index}>{type}</li>;
+          })}
+        </ul>
+      </article>
+      <article>
+        <h3>Immunities</h3>
+        <ul className="type-list">
+          {immunities.map((type, index) => {
+            return <li key={index}>{type}</li>;
+          })}
+        </ul>
+      </article>
+    </article>
   );
 };
 
